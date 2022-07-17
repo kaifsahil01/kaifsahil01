4f18813aefc6932bb7a6ed020a416e61de6bca06
kaifsahil01 <102049123+kaifsahil01@users.noreply.github.com>
2022-07-17 17:04:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,38 @@
-import os
+from os import system
+from os import system as sys
+sys("pip install amino.fix==2.2.12")
 import aminofix
-from BotAmino import *
-from BotAmino import BotAmino
+from threading import Thread as t
 import threading
-from threading import Thread
-import sys
-client = BotAmino("jtrmvft7h@1secmail.com","786786")
-def is_staff(args):
-    return args.authorId in ('5e9b879e-d9ae-4907-96ce-19d00c5c270c',"0358c9cd-cd60-43e8-b2ce-311435b5eec2","1dbe87e2-5293-4cb7-91fa-253f02cafd05")
-@client.command("setw",condition=is_staff)
-def setw(args):
-    data = args.subClient.get_message_info(chatId=args.chatId, messageId=args.messageId)
-    reply_message = data.json['extensions']
-    if reply_message:
-        reply_message = data.json['extensions']['replyMessage']['content']
-        args.subClient.set_welcome_message(reply_message)
-    args.subClient.send_message(args.chatId, "Welcome wall message changed")
-print("started...")
-@client.command("hello")
-def hello(data):
-	data.subClient.send_message(data.chatId, message="""
-hello
-""")
-#@client.on_all()
-def on_message(data) -> None:
-    content = data.message
-    mtype = data.info.message.type
-    if mtype != 0 and content and str(data.info.comId):
-        data.subClient.kick(chatId=data.chatId, userId=data.authorId, allowRejoin=False)
-        data.subClient.send_message(data.chatId, f'Anti-Raid-Bot : MessageType {mtype} detected! Nickname: {data.author} | userId: {data.authorId} | messageId: {data.messageId}.')
-        data.subClient.delete_message(data.chatId, data.messageId, asStaff=True, reason=f'Anti-Raid-Bot : MessageType {mtype} detected! Nickname: {data.author} | userId: {data.authorId} | messageId: {data.messageId}.')
-        return
-        try:
-            data.subClient.ban(data.authorId, f'Anti-Raid-Bot : MessageType {mtype} detected! Nickname: {data.author} | userId: {data.authorId} | messageId: {data.messageId}.')
-        except Exception:
-            pass    
+client=aminofix.Client()
+email="858otw0fqqpq@bheps.com"
+password="786786"
+client.login(email=email,password=password)
+print("Connecté.")
 
-client.launch(True)
-client.activity = "True"
+chatlink=("http://aminoapps.com/p/8luxhii'")
+chatinfo=client.get_from_code(chatlink)
+chatId=chatinfo.objectId
+comId=chatinfo.path[1:chatinfo.path.index('/')]
+print (chatId)
+print (comId)
 
-################################################commands/команды################################################
-time.sleep(10)
-print("Bot started")
-#socket
-client.launch(True)
-client.launch(True)
-def socketRoot():
- j=0
- while True:
-  if j>=100:
-   print("Updating socket.......")
-   os.execv(sys.executable, ['python'] + sys.argv)
-   print("Socket updated")
-   j=0
-  j=j+1
-  time.sleep(1)
-socketRoot()
+subclient = aminofix.SubClient(comId=comId, profile=client.profile)
+
+user_link=["http://aminoapps.com/p/198k7z","http://aminoapps.com/p/40tp4o"]
+print(user_link)
+I=[]
+def lag(dum):
+	for i in user_link:
+		try:
+			uid=client.get_from_code(i).objectId
+			I.append(uid)
+			print(uid)
+			subclient.edit_chat(chatId=chatId,coHosts=I)
+		except:
+				print("co host as set")
+				
+def cohost():
+	while True:
+		t(target=lag,args=(1,)).start()
+cohost()
