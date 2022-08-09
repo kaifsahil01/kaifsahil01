e48e91c30c2fef75752de0a77440d00f099e4110
kaifsahil01 <102049123+kaifsahil01@users.noreply.github.com>
2022-08-09 16:29:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
+import os
+from os import sys
 import samino
-import requests
-
-email = "jtrmvft7h@1secmail.com"  # email
-password = "786786"  # password
-chat = "http://aminoapps.com/p/3astkg"  # chat that you have host in it
-community = "http://aminoapps.com/c/BTS_worldz_"  # community that you want to invite the online members of it
-
-print("# Logging in")
+from time import sleep
+from os import path
+import pyfiglet
+from colored import fore, back, style, attr
+attr(0)
+print(fore.GREEN + style.BOLD)
+print(pyfiglet.figlet_format(" marshal", font="digital"))
+print(pyfiglet.figlet_format("CoHost Spam", font="small"))
+f='━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━'
 client = samino.Client()
-comId = client.get_from_link(community).comId
-chat = client.get_from_link(chat)
-client.login(email, password)
-print("# Getting all online members")
-local = samino.Local(comId)
-curators = local.get_all_users("curators").userId
-leaders = local.get_all_users("leaders").userId
-leaders.extend(curators)
-
-for i in range(0, 2100, 100):
-    users = local.get_online_users(i, 100).userId
-    if users: leaders.extend(users)
-    else: break
-
-
-def remove_host(userId, comId, chatId, headers): print("#", requests.Session().delete(f"https://service.narvii.com/api/v1/x{comId}/s/chat/thread/{chatId}/co-host/{userId}", headers=headers).json()["api:message"])
-
-
-print("# Starting\n")
-for userId in leaders: remove_host(userId, chat.comId, chat.objectId, client.headers)
+e="jtrmvft7h@1secmail.com"
+p="786786"
+client.login(e,p)
+print("\nLogin Successfully")
+print(f)
+#keep_alive()
+client.headers
+HostChatLink="http://aminoapps.com/p/3astkgt"
+config_chat = client.get_from_link(HostChatLink).json #You have  host on chat
+thread_id = config_chat["linkInfo"]["objectId"]
+ndc_id = config_chat["linkInfo"]["ndcId"]
+local = samino.Local(comId = ndc_id)
+targetlink=["http://aminoapps.com/p/whcet8u","http://aminoapps.com/p/t9xses","http://aminoapps.com/p/if5i3l"]
+remove = client.get_from_link(str(targetlink)).objectId
+while True:
+	print("boom.....")
+	try:
+		local.remove_host(chatId=thread_id, userId=remove)
+	except:
+		pass
